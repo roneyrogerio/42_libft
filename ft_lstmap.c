11dc1c4f047904912f21917a6d278bef24ce4b8e
Roney <roneyrogerio92@gmail.com>
2020-07-10 09:26:24 -0300

    improve code: remove unused code.

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: rde-oliv <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/11 18:42:05 by rde-oliv          #+#    #+#             */
-/*   Updated: 2020/07/10 06:00:59 by rde-oliv         ###   ########.fr       */
+/*   Updated: 2020/07/10 09:24:10 by rde-oliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,10 +27,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 			ft_lstclear(&new_lst, del);
 			break ;
 		}
-		if (!new_lst)
-			new_lst = elem;
-		else
-			ft_lstadd_back(&new_lst, elem);
+		ft_lstadd_back(&new_lst, elem);
 		lst = lst->next;
 	}
 	return (new_lst);
